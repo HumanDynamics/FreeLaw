63a86212b03765bb1b6c507b7558782463eeddeb
captainkark <captainkark@users.noreply.github.com>
2014-10-20 17:00:23 -0400

    Update test_formPro.php

--- IdeaEngine/test_formPro.php
+++ IdeaEngine/test_formPro.php
@@ -1,24 +1,37 @@
-<?php
-
-if (isset($_POST['name'])) {
-    $name = $_POST['name'];
-} else {
-    $name = '';
-}
-
-if (isset($_POST['email'])) {
-    $email = $_POST['email'];
-} else {
-    $email = '';
-}
-
-if (isset($_POST['law'])) {
-    $law = $_POST['law'];
-} else {
-    $law = '';
-}
-
-echo $name.' '.$email.' '.$law;
-
-///insert into sql database here
+	
+<?php	include("conn.php");
+			
+	if (isset($_GET['name'])) {
+		$name = $_GET['name'];
+	} else {
+		$name = '';
+	}
+	
+	if (isset($_GET['email'])) {
+		$email = $_GET['email'];
+	} else {
+		$email = '';
+	}
+	
+	if (isset($_GET['law'])) {
+		$law = $_GET['law'];
+	} else {
+		$law = '';
+	}
+	
+	//echo $name.' '.$email.' '.$law;
+		
+	
+	$sql="INSERT INTO TEST
+	(name, email, law, headline, rate_headline, blurb, rate_blurb)
+		VALUES 
+	('$name', '$email', '$law', '', '', '', '' )";
+	
+	$rs=odbc_exec($conn,$sql);
+	if (!$rs) {exit("Error in SQL");} 
+	
+	
+	//$Message = 'Submitted successfully';
+	//header("Location:test_form.php?Message=".$Message);
+	
 ?>
