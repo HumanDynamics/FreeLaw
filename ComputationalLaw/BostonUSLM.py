094638822a9eb2b7082e4cf266c1cb05319eb2a1
shrey13 <sanalp139@dhcp-18-111-92-103.dyn.mit.edu>
2014-11-22 14:57:36 -0500

    Updating Python Script

--- ComputationalLaw/BostonUSLM.py
+++ ComputationalLaw/BostonUSLM.py
@@ -2,14 +2,13 @@
 Licensed under Creative Commons: Attribution 4.0 International (CC BY 4.0)
 http://creativecommons.org/licenses/by/4.0/
 """
-
 import xml.etree.ElementTree as ET
 import requests
 import os
 import re
 
 TEST_URL = 'https://raw.github.com/HumanDynamics/FreeLaw/master/ExperimentsAndExamples/USCode-03.xml'
-URL = 'https://raw.github.com/HumanDynamics/FreeLaw/master/USLM%20US%20Code/usc02_test.xml'
+URL = 'https://raw.github.com/HumanDynamics/FreeLaw/master/USLM-US-Code/usc02.xml'
 r = requests.get(URL)
 text = r.text.encode('utf-8')
 ##text = text.lower()
@@ -20,8 +19,53 @@ tree = ET.ElementTree(ET.fromstring(text))
 namespace = ".//{http://xml.house.gov/schemas/uslm/1.0}"
 
 
-ids = ["id3cd25b8f-2e14-11e4-9af5-e14406bc0d92","id3cd25b93-2e14-11e4-9af5-e14406bc0d92","id3cd4ccb5-2e14-11e4-9af5-e14406bc0d92"]
+ids = ["id3ccda143-2e14-11e4-9af5-e14406bc0d92","id3cd25b8f-2e14-11e4-9af5-e14406bc0d92","id3cd25b93-2e14-11e4-9af5-e14406bc0d92","id3cd4ccb5-2e14-11e4-9af5-e14406bc0d92"]
 
+title = tree.find(namespace + "title")
+titleNum = title.find(namespace + "num")
+print "Title",titleNum.attrib["value"]
+print title.find(namespace + "heading").text
+
+chapterList = tree.findall(namespace + "chapter")
+counter = 0
+for chapter in chapterList:
+    counter += 1
+    output = ""
+    chapterNum = chapter.find(namespace + "num").attrib["value"]
+    chapterHeading = chapter.find(namespace + "heading").text
+    output += "\nChapter "
+    output += chapterNum + "\n"
+    output += chapterHeading + "\n"
+    section = chapter.findall(namespace + "section")
+    
+    for section in chapter:
+        if section.tag == ("{http://xml.house.gov/schemas/uslm/1.0}section"):            
+            title = section.find(namespace + "heading")
+            output += title.text + "\n"
+            
+            paragraph = None
+            for para in section:
+                if para.tag == ("{http://xml.house.gov/schemas/uslm/1.0}content"):
+                    paragraph = para.find(namespace + "p")
+
+            if paragraph != None:
+                output +=  paragraph.text + "\n"
+                                
+            subsectionList = section.findall(namespace + "subsection")    
+            for subsection in subsectionList:
+                for subsectionText in subsection.itertext():
+                    output += subsectionText + "\n"
+    print output
+
+print "Number of Chapters",counter
+"""
+                if subsection != None:
+                    subsectionText = subsection.findall(namespace + "p")
+                    for p in subsectionText:
+                        print p.text
+                    """
+
+"""
 section = tree.findall(namespace + "section")
 ##for elt in tree.getiterator():
 for sec in section:
@@ -32,9 +76,7 @@ for sec in section:
         print "\n" + title.text
         print para.text
         print sourceCredit.text
-
-
-
+"""
         
 ##attributes = section.attrib
 ##sectionID = attributes.get('id',-1)
