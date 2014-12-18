fc8093d5133097dece8aa7d71b0858b11cb965a9
Keller Scholl <keller.scholl@gmail.com>
2014-12-18 00:30:34 -0500

    Added PMI and other relevant functions. Things are now useable.

--- relativeFrequency.py
+++ relativeFrequency.py
@@ -2,12 +2,9 @@ from nltk.tokenize import sent_tokenize,word_tokenize
 from nltk.corpus import stopwords
 from collections import defaultdict
 from string import punctuation
-from heapq import nlargest
-import re
-import codecs
 
 class FrequencySummarizer:
-    def __init__(self, low_thresh=0.1, high_thresh=0.9,text):
+    def __init__(self, low_thresh=0.2, high_thresh=0.7,text):
         """
          Initialize the text summarizer.
          Words that have a frequency term lower than low_thresh 
@@ -20,6 +17,7 @@ class FrequencySummarizer:
         self._stopwords = set(stopwords.words('english') + list(punctuation) + list(ignore))
         self.text = unicode(text, errors='ignore')
         self.sents = sent_tokenize(self.text)
+        self._frequencies = {}
 
     def _compute_frequencies(self, word_tk):
         """Input: tokenized words.
@@ -45,7 +43,11 @@ class FrequencySummarizer:
         return self._compute_frequencies(word_tk)
 
     def sentiment(self):
-        """Checks the sentiment of the text document. IMPORTANT! MAXIMUM 80,000 CHARACTERS!"""
+        """Checks the sentiment of the text document. 
+        IMPORTANT! MAXIMUM 80,000 CHARACTERS!
+        Note: this relies on being able to do an os call, and having installed
+        pexpect. pexpect is not a standard part of python, which is why the
+        import is kept here, away from the critical parts."""
         from pexpect import run as OSCall
         string = 'curl -d "text=' + self.text + '" http://text-processing.com/api/sentiment/'
         x = OSCall(string)
@@ -54,10 +56,51 @@ class FrequencySummarizer:
         result = {'pos':'positive','neutral':'neutral','neg':'negative'}[y]+" "+str(loads(x)['probability'][y])
         return result
 
+    def frequency(self,word):
+        """Returns the fraction of sentences that contain this word. Autocaches."""
+        if not self._frequencies.has_key(word):
+            n = len(self.sents)
+            self._frequences[word]=sum([1 if word.lower() in sent.lower(
+                ) else 0 for sent in self.sents])/n
+        return self._frequencies[word]
+
+    def jointFrequency(self,wordList,suppressWarnings=0):
+        """Returns the number of sentences in which all words in wordList.
+        wordList functions best if handed a list of strings."""
+        total = 0
+        # This function makes liberal abuse of the fact that 0 is false.
+        if not len(wordList) and not suppressWarnings:
+            print("Warning: wordList is empty.")
+        for sent in self.sents():
+            # Forgive me Guido, for I have used a for-else loop.
+            for word in wordList:
+                if word.lower() in sent.lower():
+                    break
+            else:
+                total+=1
+        if not total and not suppressWarnings:
+            print("Warning:"+wordList+"is not found in any sentences.")
+        return total/len(sents)
+
+    def PMI(self,firstWord,secondWord):
+        """PMI, or pointwise mutual information, is a good way of representing
+        how much too words like or dislike each other. It divides
+        the probability of both words appearing in a sentence by their 
+        independent probabilities, and takes that log. Interpretation taken
+        from https://slackprop.wordpress.com/2013/06/03/on-geek-versus-nerd/ .
+        """
+        firstFrequency = self.frequency(firstWord)
+        secondFrequency = self.frequency(secondWord)
+        jointFrequency = self.jointFrequency([firstWord,secondWord])
+        from math import log
+        return log(jointFrequency/(firstFrequency*secondFrequency))
+
 if __name__ == "__main__":
+    #May not work with windows.
     x = open("ComputationalLaw/Complete US Code/CompleteUSCode.txt").read()
     y=x[:len(x)/5]
     n = FrequencySummarizer(y)
     a = n.cooccurence("secretary")
     b = n.cooccurence("congress")
-    print(n.sentiment())
+    c = n.PMI("secretary","shall")
+    #print(n.sentiment())
